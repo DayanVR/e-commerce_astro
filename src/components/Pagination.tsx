82e793ba6f62ef2cc4fb52db0009f26bcb525e76
Dayan Villavicencio Rolffot <108312281+DayanVR@users.noreply.github.com>
2025-01-26 16:31:25 -0500

    better structured

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -18,7 +18,8 @@ export default function Pagination() {
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
+            Showing <span className="font-medium">1</span> to{" "}
+            <span className="font-medium">10</span> of{" "}
             <span className="font-medium">97</span> results
           </p>
         </div>
@@ -85,10 +86,7 @@ export default function Pagination() {
                 viewBox="0 0 448 512"
                 className="svg-inline--fa fa-arrow-right fa-w-14 fa-7x"
               >
-                <path
-                  
-                  d="M218.101 38.101L198.302 57.9c-4.686 4.686-4.686 12.284 0 16.971L353.432 230H12c-6.627 0-12 5.373-12 12v28c0 6.627 5.373 12 12 12h341.432l-155.13 155.13c-4.686 4.686-4.686 12.284 0 16.971l19.799 19.799c4.686 4.686 12.284 4.686 16.971 0l209.414-209.414c4.686-4.686 4.686-12.284 0-16.971L235.071 38.101c-4.686-4.687-12.284-4.687-16.97 0z"
-                ></path>
+                <path d="M218.101 38.101L198.302 57.9c-4.686 4.686-4.686 12.284 0 16.971L353.432 230H12c-6.627 0-12 5.373-12 12v28c0 6.627 5.373 12 12 12h341.432l-155.13 155.13c-4.686 4.686-4.686 12.284 0 16.971l19.799 19.799c4.686 4.686 12.284 4.686 16.971 0l209.414-209.414c4.686-4.686 4.686-12.284 0-16.971L235.071 38.101c-4.686-4.687-12.284-4.687-16.97 0z"></path>
               </svg>
             </span>
           </a>
